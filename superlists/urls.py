e05bee95b8243b55b2e428b9eb3621d3167bc3e8
Thiago Melo <thiago.lc.melo@gmail.com>
2018-10-11 23:24:27 -0300

    new URL + view for adding to existing lists. FT passes :-)

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from lists.views import home_page, view_list, new_list
+from lists.views import home_page, view_list, new_list, add_item
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name='home'),
-    path('lists/the-only-list-in-the-world/', view_list, name='view_list'),
+    path('lists/<int:list_id>/', view_list, name='view_list'),
+    path('lists/<int:list_id>/add_item', add_item, name='add_item'),
     path('lists/new', new_list, name='new_list'),
 ]
