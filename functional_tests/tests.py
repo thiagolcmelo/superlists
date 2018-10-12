88c7401f91cbef96478f10b081918c5ae8809dca
Thiago Melo <thiago.lc.melo@gmail.com>
2018-10-12 20:33:28 -0300

    use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,11 +3,12 @@
 import time
 import unittest
 
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -45,7 +46,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)  # wait for refresh
+        time.sleep(1)  # wait for refresh
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_fow_row_in_list_table('1: Buy peacock feathers')
@@ -58,7 +59,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # the page updates again, and now shows both items on her lists
-        time.sleep(3)  # wait for refresh
+        time.sleep(1)  # wait for refresh
         self.check_fow_row_in_list_table('1: Buy peacock feathers')
         self.check_fow_row_in_list_table(
             '2: Use peacock feathers to make a fly'
@@ -84,7 +85,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)  # wait for refresh
+        time.sleep(1)  # wait for refresh
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
@@ -97,3 +98,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)  # wait for refresh
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
