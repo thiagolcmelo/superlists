b9174bffdc640b109e55ab25b3d8b1a38e67914d
Thiago Melo <thiago.lc.melo@gmail.com>
2018-11-06 19:32:36 -0200

    finally validating unique

--- lists/forms.py
+++ lists/forms.py
@@ -24,7 +24,7 @@ class ItemForm(forms.models.ModelForm):
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
-
+    
 class ExistingListItemForm(ItemForm):
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -36,3 +36,6 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+    
+    def save(self):
+        return forms.models.ModelForm.save(self)
